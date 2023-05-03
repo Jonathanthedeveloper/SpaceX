7ae9b8943335dd156b0126e0d70a5275478fb1a6
ZedPriest <Charleschukwuemeka47@gmail.com>
2023-05-03 08:52:16 +0100

    News Update

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,7 +1,10 @@
 const Post = require("../models/news.model");
 
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  `SpaceX Completes Successful Launch of Crew-2 Mission to International Space Station Elon Musk's space exploration company, SpaceX, has completed the successful launch of its Crew-2 mission to the International Space Station (ISS). The mission, which launched on April 23, 2021, was the second operational flight of SpaceX's Crew Dragon spacecraft and the first to fly with two international partner astronauts from the European Space Agency and the Japan Aerospace Exploration Agency. 
+  The Crew-2 mission marks another significant milestone for SpaceX, which has been working to revolutionize space travel and make it more accessible to the public. 
+  It also highlights the company's ongoing collaboration with NASA and other international space agencies to advance the cause of space exploration. During the Crew-2 mission, the four-person crew will spend approximately six months aboard the ISS, conducting a variety of scientific experiments and performing maintenance tasks. 
+  This will be the longest duration human spaceflight launched from the United States, and it will enable NASA and its partners to gain valuable insights into how the human body reacts to long-duration spaceflight. Overall, the success of the Crew-2 mission is a testament to SpaceX's commitment to innovation and its ability to work collaboratively with others in pursuit of a common goal. As the company continues to push the boundaries of space exploration, it is likely that we will see more groundbreaking achievements in the years to come.`;
 
 class NewsHandler {
     
--- models/news.model.js
+++ models/news.model.js
@@ -3,6 +3,7 @@ const { Schema, model, default: mongoose } = require('mongoose');
 const postSchema = new Schema ({
     title: String,
     content: String,
+    date: { type: Date, default: Date.now }
   });
 
 const Post = model("Post", postSchema);
